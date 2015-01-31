6ed92434ff877a0019c69060e9d9980b2d157015
joskarthic <joskarthick3@gmail.com>
2015-01-31 15:49:40 +0530

    change confif.js

--- www/js/config.js
+++ www/js/config.js
@@ -2,12 +2,16 @@
 var QBPARAMS = {
 	app_id      : 19101,
 	auth_key    : 'CFKVXVZDUt7dE-s',
-	auth_secret : 't3n7fr4pGKAhbC5'
+	auth_secret : 't3n7fr4pGKAhbC5',
+	debug: false
 }
 
 //Chat params
 var CHAT = {
 	server      : 'chat.quickblox.com',
 	bosh_url    : 'http://chat.quickblox.com:5280',
-	roomJID     : '3907_test@muc.chat.quickblox.com'
+	roomJID     : '19101_54cca338535c122b750001b6@muc.chat.quickblox.com'
 }
+
+
+
